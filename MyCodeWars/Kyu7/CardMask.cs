6a88502ce6a179d358612d66360081ecf8e3ec17
elaynne torres <elaynne.ufs@gmail.com>
2019-03-31 23:03:13 -0300

    initial commit

---
+++ MyCodeWars/Kyu7/CardMask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyCodeWars
+{
+    public static class CardMask
+    {
+        /*
+        Kata.Maskify('4556364607935616'); // should return "############5616"
+        Kata.Maskify('64607935616');      // should return "#######5616"
+        Kata.Maskify('1');                // should return "1"
+        Kata.Maskify('');                 // should return ""
+
+        // "What was the name of your first pet?"
+        Kata.Maskify('Skippy');                                   // should return "##ippy"
+        Kata.Maskify('Nananananananananananananananana Batman!'); // should return "####################################man!"
+        */
+
+        // return masked string
+        public static string Maskify(string cc)
+        {
+            if (cc == "1" || cc == "") return cc;
+            string str = "";
+
+            for (int i = 0; i < cc.Length; i++)
+            {
+                str += ((i < cc.Length - 4) ? "#" : cc[i].ToString());
+            }
+
+            return str;
+        }
+    }
+}
---
+++ MyCodeWars/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyCodeWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
